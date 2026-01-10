a5665830d96d9d07a0b16d65c9565d4d725409c0
luvis-sirpi <luvis@sirpi.io>
2026-01-10 18:12:51 +0530

    chore: added commad description

--- cmd/add.go
+++ cmd/add.go
@@ -8,13 +8,11 @@ import (
 
 var addCmd = &cobra.Command{
 	Use:   "add",
-	Short: "A brief description of your command",
+	Short: "This command is used to add your tasks",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+tasktraker-cli add "First write function for add"`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("add called")
 	},
