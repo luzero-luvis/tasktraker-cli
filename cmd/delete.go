459696d50cd2b0e260399571eff9012347bb9bfc
luvis-sirpi <luvis@sirpi.io>
2026-01-12 22:41:55 +0530

    feat: added delete commad

---
+++ cmd/delete.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"tesktracker-cli/internal"
+
+	"github.com/spf13/cobra"
+)
+
+var deleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete a task by ID",
+	Long:  `Delete a task from the list. Usage: ./tasktraker-cli delete <id>`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Printf("%s", "please provide the valid id")
+			os.Exit(1)
+		}
+
+		id, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Printf("%s", "invalid task id")
+		}
+
+		tasks, err := internal.LoadTask()
+		if err != nil {
+			fmt.Printf("%s", "error loading tasks")
+		}
+
+		found := false
+		var UpdatedTask []internal.Task
+		for _, tasks := range tasks {
+			if tasks.ID == id {
+				found = true
+			} else {
+				UpdatedTask := append(UpdatedTask, tasks)
+				_ = UpdatedTask
+			}
+		}
+
+		if !found {
+			fmt.Printf("%s", "that id you are searching is not found")
+		}
+
+		if err := internal.SaveTask(UpdatedTask); err != nil {
+			fmt.Printf("error saving tasks %v\n", err)
+		} else {
+			fmt.Printf("%s\n", "task deleted successfully")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deleteCmd)
+}
