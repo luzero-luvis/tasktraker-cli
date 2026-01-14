c7f19eab23d9bfb65a8e17819f8403481bda69c6
luvis-sirpi <luvis@sirpi.io>
2026-01-14 23:47:31 +0530

    fix: added better formate to print feat: to list baseed on progress

--- cmd/list.go
+++ cmd/list.go
@@ -28,30 +28,47 @@ tasktracker-cli list`,
 			fmt.Println("list is empty")
 			os.Exit(1)
 		}
-
+		fmt.Println("ID | Description | Status       | CreatedAt")
+		fmt.Println("---+-------------+--------------+---------------------------")
 		if len(args) > 0 {
+			arg := args[0]
 			id, err := strconv.Atoi(args[0])
 			if err != nil {
-				fmt.Printf("%s", "please provide valide id or id not exists")
+				if arg != "todo" && arg != "done" && arg != "in-progess" {
+					fmt.Printf("%s", "please provide valide id or id not exists")
+				}
 			}
 
 			for _, tasks := range tasks {
 				if tasks.ID == id {
-					fmt.Printf("%d\t", tasks.ID)
-					fmt.Printf("%s\t\t", tasks.Description)
-					fmt.Printf("%v\t", tasks.Status)
-					fmt.Printf("%v\t", tasks.CreatedAt)
-					fmt.Printf("%v\t\n", tasks.UpdatedAt)
+					fmt.Printf("%d  | %s  | %v  | %v  | %v\n",
+						tasks.ID,
+						tasks.Description,
+						tasks.Status,
+						tasks.CreatedAt,
+						tasks.UpdatedAt)
 					return
 				}
 			}
 		} else {
 			for _, tasks := range tasks {
-				fmt.Printf("%d\t", tasks.ID)
-				fmt.Printf("%s\t\t", tasks.Description)
-				fmt.Printf("%v\t", tasks.Status)
-				fmt.Printf("%v\t", tasks.CreatedAt)
-				fmt.Printf("%v\t\n", tasks.UpdatedAt)
+				fmt.Printf("%d  | %s  | %v  | %v  | %v\n",
+					tasks.ID,
+					tasks.Description,
+					tasks.Status,
+					tasks.CreatedAt,
+					tasks.UpdatedAt)
+			}
+		}
+
+		if len(args) == 0 {
+			return
+		}
+
+		filterSatus := args[0]
+		for _, tasks := range tasks {
+			if tasks.Status == filterSatus {
+				fmt.Printf("%d | %s | %v | %v | %v |\n", tasks.ID, tasks.Description, tasks.Status, tasks.CreatedAt, tasks.UpdatedAt)
 			}
 		}
 	},
